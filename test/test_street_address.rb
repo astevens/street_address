8d9699f8e67e33d37f335d4e6c4338d6688cfc43
cooperq <cooper@radicaldesigns.org>
2012-08-28 14:27:02 -0700

    adding tests for street address library

--- test/test_street_address.rb
+++ test/test_street_address.rb
@@ -1,7 +1,27 @@
-require 'helper'
+require './helper'
 
 class TestStreetAddress < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+
+  should "parse addresses in the format of '123 fake st' the same as it parses '123 fake st.'" do
+    without_period = StreetAddress.parse('123 fake st')
+    with_period = StreetAddress.parse('123 fake st.')
+    assert_equal with_period, without_period
+    assert_equal without_period, {"number"=>"123", "prefix"=>"", "street"=>"fake", "type"=>"st", "suffix"=>"", "city"=>"", "state"=>"", "zip"=>""}
+  end
+
+  should "parse addresses in the format of '123 fake st n' the same as it parses '123 fake st. n'" do
+    without_period = StreetAddress.parse('123 noperiod st n')
+    with_period = StreetAddress.parse('123 period st. n')
+    assert_equal({"number"=>"123", "prefix"=>"", "street"=>"noperiod", "type"=>"st", "suffix"=>"n", "city"=>"", "state"=>"", "zip"=>""}, without_period)
+    assert_equal({"number"=>"123", "prefix"=>"", "street"=>"period", "type"=>"st", "suffix"=>"n", "city"=>"", "state"=>"", "zip"=>""}, with_period)
+  end
+
+  should "parse addresses of various formats" do
+    assert_equal({"number"=>"123", "prefix"=>"e", "street"=>"fake", "type"=>"st", "suffix"=>"n", "city"=>"santa rosa", "state"=>"ca", "zip"=>"94410"}, StreetAddress.parse('123 e fake st n, santa rosa, ca, 94410'))
+    assert_equal({"number"=>"123", "prefix"=>"e", "street"=>"fake", "type"=>"st", "suffix"=>"n", "city"=>"santa rosa", "state"=>"CA", "zip"=>"94410"}, StreetAddress.parse('123 e fake st n, santa rosa, CALIFORNIA 94410'))
+  end
+
+  should "parse addresses with arbitrary spacing" do
+    assert_equal({"number"=>"123", "prefix"=>"e", "street"=>"fake", "type"=>"st", "suffix"=>"n", "city"=>"santa rosa", "state"=>"ca", "zip"=>"94410"}, StreetAddress.parse('123    e.    fake   st.   n.,   santa rosa,    ca     94410'))
   end
 end
